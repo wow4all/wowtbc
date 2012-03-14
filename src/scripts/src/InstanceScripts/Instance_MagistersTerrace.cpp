eb364b8a79f83e6fd07f2b7f78d850778dbb2a45
clarkylord <clarkylord@hotmail.co.uk>
2012-03-14 02:53:45 +0000

    Scripts/Magister's Terrace:Implement base for Kael'thas

--- src/scripts/src/InstanceScripts/Instance_MagistersTerrace.cpp
+++ src/scripts/src/InstanceScripts/Instance_MagistersTerrace.cpp
@@ -632,23 +632,98 @@ class ZelfanAI : public MoonScriptCreatureAI
 
 };
 
-#define BOSS_Kaelthas 24664
-#define KAELTHAS_FIREBALL 44189
-#define KAELTHAS_PHOENIX 44194
-//#define KAELTHAS_FLAME_STRIKE 46162
-#define KAELTHAS_GRAVITY_LAPSE 44224
+/*Corrupt Kael'thas
 
+/*** Spells ***/
+
+// Phase 1 spells
+#define SPELL_FIREBALL_NORMAL 44189 // Deals 2700-3300 damage at current target
+#define SPELL_FIREBALL_HEROIC 46164 // 4950-6050
+
+#define SPELL_PHOENIX 44194 // Summons a phoenix (Doesn't work?)
+#define SPELL_PHOENIX_BURN 44197 // A spell Phoenix uses to damage everything around
+#define SPELL_REBIRTH_DMG 44196 // DMG if a Phoenix rebirth happen
+
+#define SPELL_FLAMESTRIKE1_NORMAL 44190 // Damage part
+#define SPELL_FLAMESTRIKE1_HEROIC 46163 // Heroic damage part
+#define SPELL_FLAMESTRIKE2 44191 // Flamestrike indicator before the damage
+#define SPELL_FLAMESTRIKE3 44192 // Summons the trigger + animation (projectile)
+
+#define SPELL_SHOCK_BARRIER 46165 // Heroic only; 10k damage shield, followed by Pyroblast
+#define SPELL_PYROBLAST 36819 // Heroic only; 45-55k fire damage
+
+// Phase 2 spells
+#define SPELL_GRAVITY_LAPSE_INITIAL 44224 // Cast at the beginning of every Gravity Lapse
+#define SPELL_GRAVITY_LAPSE_CHANNEL 44251 // Channeled; blue beam animation to every enemy in range
+#define SPELL_TELEPORT_CENTER 44218 // Should teleport people to the center. Requires DB entry in spell_target_position.
+#define SPELL_GRAVITY_LAPSE_FLY 44227 // Hastens flyspeed and allows flying for 1 minute. For some reason removes 44226.
+#define SPELL_GRAVITY_LAPSE_DOT 44226 // Knocks up in the air and applies a 300 DPS DoT.
+#define SPELL_ARCANE_SPHERE_PASSIVE 44263 // Passive auras on Arcane Spheres
+#define SPELL_POWER_FEEDBACK 44233 // Stuns him, making him take 50% more damage for 10 seconds. Cast after Gravity Lapse
+
+#define BOSS_Kaelthas 24664
+/*** Creatures ***/
+#define CREATURE_PHOENIX 24674
+#define CREATURE_PHOENIX_EGG 24675
+#define CREATURE_ARCANE_SPHERE 24708
+#define CREATURE_FELKAEL_FLAMESTRIKE 24666
 
 class KaelThasMTAI : public MoonScriptBossAI
 {
 	MOONSCRIPT_FACTORY_FUNCTION(KaelThasMTAI, MoonScriptBossAI);
 	KaelThasMTAI(Creature* pCreature) : MoonScriptBossAI(pCreature)
 	{
-		//AddPhaseSpell(1, AddSpell(FELMYST_CLEAVE, Target_Current, 6, 0, 10, 0, 5));
-       AddPhaseSpell(1, AddSpell(KAELTHAS_FIREBALL, Target_RandomPlayer, 40, 2, 6, 0, 40));
+	  
+       AddPhaseSpell(1, AddSpell(SPELL_FIREBALL_NORMAL, Target_RandomPlayer, 40, 2, 6, 0, 40));
 
-	   
+	   Phases = 3;
+
 	}
+	
+	void OnDied(Unit * mKiller)
+	{
+	
+	}
+	
+	void OnTargetDied(Unit* mTarget)
+	{
+	
+	}
+	
+	void OnCombatStart(Unit* mTarget)
+	{
+	
+	}
+	
+	uint32 Phases; 
+
+};
+
+class mob_felkael_flamestrikeAI : public MoonScriptCreatureAI
+{
+	MOONSCRIPT_FACTORY_FUNCTION(mob_felkael_flamestrikeAI, MoonScriptCreatureAI);
+	mob_felkael_flamestrikeAI(Creature* pCreature) : MoonScriptCreatureAI(pCreature)
+	{
+	  
+
+	}
+	
+	void OnDied(Unit * mKiller)
+	{
+	
+	}
+	
+	void OnTargetDied(Unit* mTarget)
+	{
+	
+	}
+	
+	void OnCombatStart(Unit* mTarget)
+	{
+	
+	}
+	
+
 };
 
 void SetupMagistersTerrace(ScriptMgr* pScriptMgr) 
@@ -673,5 +748,6 @@ void SetupMagistersTerrace(ScriptMgr* pScriptMgr)
 	pScriptMgr->register_creature_script(CN_ZELFAN, &ZelfanAI::Create);
 	
 	pScriptMgr->register_creature_script(BOSS_Kaelthas, &KaelThasMTAI::Create);
+	pScriptMgr->register_creature_script(CREATURE_FELKAEL_FLAMESTRIKE, &mob_felkael_flamestrikeAI::Create);
 }
 
